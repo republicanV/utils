59d333fa44af7f796c3ed8ae7544cf070aa92dd0
Vadym Sapovskyi <vsapovskyi@homeaway.com>
2019-07-05 13:53:52 +0300

    fix

--- link-detector.js
+++ link-detector.js
@@ -1,4 +1,4 @@
 const detectLink = (text) => {
 	const matchTemplate = /\b(?:(?:https?|ftp|http):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i;
-	return !!matchTemplate.test(text);
+	return matchTemplate.test(text);
 }
